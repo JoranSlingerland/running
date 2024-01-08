b5a2120c85b7bd0c13610a73ff3194f018fba86f
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2024-01-08 19:32:53 +0100

    Remove unused code for MetaItem component

--- pages/authenticated/calendar.tsx
+++ pages/authenticated/calendar.tsx
@@ -120,16 +120,6 @@ function MetaItem({
   selectedSport: string | null;
   setSelectedSport: (value: string) => void;
 }): JSX.Element {
-  const data = [
-    264, 417, 438, 887, 309, 397, 550, 575, 563, 430, 525, 592, 492, 467, 513,
-    546, 983, 340, 539, 243, 226, 192,
-  ].map((value, index) => ({ value, index }));
-  const config = {
-    data,
-    padding: 8,
-    xField: 'index',
-    yField: 'value',
-  };
   return (
     <div>
       <div className="ml-2">
