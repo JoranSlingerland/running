0cbe8abea01a56865ccce82f3d500f7f3f0c0011
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2024-04-21 21:55:57 +0200

    Add request logger

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,9 +1,23 @@
+import { NextFetchEvent } from 'next/server';
 import { withAuth } from 'next-auth/middleware';
+import { NextMiddlewareWithAuth } from 'next-auth/middleware';
+import { NextRequestWithAuth } from 'next-auth/middleware';
 
-export default withAuth({
-  pages: {
-    signIn: '/',
-  },
-});
+const requestLogger =
+  (handler: NextMiddlewareWithAuth) =>
+  async (req: NextRequestWithAuth, res: NextFetchEvent) => {
+    console.log(
+      `\x1b[35m[New Request]\x1b[0m \x1b[36m[${new Date().toISOString()}]\x1b[0m \x1b[32m${req.method}\x1b[0m \x1b[33m${req.nextUrl.href}\x1b[0m`,
+    );
+    return handler(req, res);
+  };
+
+export default requestLogger(
+  withAuth({
+    pages: {
+      signIn: '/',
+    },
+  }),
+);
 
 export const config = { matcher: ['/authenticated/:page*'] };
