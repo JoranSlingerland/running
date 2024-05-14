653a39b6735981422f88e17c729d2e271b0f6856
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2024-05-14 19:44:22 +0200

    Refactor file structure

--- packages/env/src/env.ts
+++ packages/env/src/env.ts
@@ -1,44 +1,13 @@
 import { z } from 'zod';
 
-export const frontendEnv = z.object({
-  API_SHARED_KEY: z.string().min(1),
-  STRAVA_CLIENT_ID: z.string().min(1),
-  STRAVA_CLIENT_SECRET: z.string().min(1),
-  STRAVA_DAILY_LIMIT: z.coerce.number().int().positive(),
-  STRAVA_15MIN_LIMIT: z.coerce.number().int().positive(),
-  MONGO_INITDB_ROOT_USERNAME: z.string().min(1),
-  MONGO_INITDB_ROOT_PASSWORD: z.string().min(1),
-  MONGODB_URI: z.string().min(1),
-  NEXTAUTH_ADMIN_EMAIL: z.string().email(),
-  NEXTAUTH_SECRET: z.string().min(1),
-  NEXTAUTH_SALT: z.string().min(1),
-  NEXTAUTH_URL: z.string().url(),
-  NEXTAUTH_GITHUB_CLIENTID: z.string().min(1),
-  NEXTAUTH_GITHUB_CLIENTSECRET: z.string().min(1),
-  NESTJS_URL: z.string().url(),
-  MAPTILERTOKEN: z.string().min(1),
-  NODE_ENV: z.string().min(1),
-});
+import { validateEnv } from './helpers';
 
-export const frontendClientEnv = z.object({
-  NEXT_PUBLIC_api_key: z.string().min(1),
-});
+const base64regex =
+  /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 
-export const backendEnv = z.object({
-  API_SHARED_KEY: z.string().min(1),
-  STRAVA_CLIENT_ID: z.string().min(1),
-  STRAVA_CLIENT_SECRET: z.string().min(1),
-  STRAVA_DAILY_LIMIT: z.coerce.number().int().positive(),
-  STRAVA_15MIN_LIMIT: z.coerce.number().int().positive(),
-  MONGO_INITDB_ROOT_USERNAME: z.string().min(1),
-  MONGO_INITDB_ROOT_PASSWORD: z.string().min(1),
-  MONGODB_URI: z.string().min(1),
-  NODE_ENV: z.string().min(1),
-});
-
-export const backendEnvFunction = () => {
-  const backendEnv = z.object({
-    API_SHARED_KEY: z.string().min(1),
+export const frontendServerEnv = validateEnv(
+  z.object({
+    API_SHARED_KEY: z.string().length(44).regex(base64regex, 'Invalid base64'),
     STRAVA_CLIENT_ID: z.string().min(1),
     STRAVA_CLIENT_SECRET: z.string().min(1),
     STRAVA_DAILY_LIMIT: z.coerce.number().int().positive(),
@@ -46,12 +15,34 @@ export const backendEnvFunction = () => {
     MONGO_INITDB_ROOT_USERNAME: z.string().min(1),
     MONGO_INITDB_ROOT_PASSWORD: z.string().min(1),
     MONGODB_URI: z.string().min(1),
-    NODE_ENV: z.string().min(1),
-  });
+    NEXTAUTH_ADMIN_EMAIL: z.string().email(),
+    NEXTAUTH_SECRET: z.string().length(44).regex(base64regex, 'Invalid base64'),
+    NEXTAUTH_SALT: z.string().length(44).regex(base64regex, 'Invalid base64'),
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_GITHUB_CLIENTID: z.string().min(1),
+    NEXTAUTH_GITHUB_CLIENTSECRET: z.string().min(1),
+    NESTJS_URL: z.string().url(),
+    MAPTILERTOKEN: z.string().min(1),
+    NODE_ENV: z.enum(['development', 'test', 'production']),
+  }),
+);
 
-  const parsedEnv = backendEnv.safeParse(process.env);
-  if (!parsedEnv.success) {
-    throw new Error(parsedEnv.error.errors.join('\n'));
-  }
-  return parsedEnv.data;
-};
+export const frontendClientEnv = validateEnv(
+  z.object({
+    NEXT_PUBLIC_api_key: z.string().min(1),
+  }),
+);
+
+export const backendEnv = validateEnv(
+  z.object({
+    API_SHARED_KEY: z.string().length(44).regex(base64regex, 'Invalid base64'),
+    STRAVA_CLIENT_ID: z.string().min(1),
+    STRAVA_CLIENT_SECRET: z.string().min(1),
+    STRAVA_DAILY_LIMIT: z.coerce.number().int().positive(),
+    STRAVA_15MIN_LIMIT: z.coerce.number().int().positive(),
+    MONGO_INITDB_ROOT_USERNAME: z.string().min(1),
+    MONGO_INITDB_ROOT_PASSWORD: z.string().min(1),
+    MONGODB_URI: z.string().min(1),
+    NODE_ENV: z.enum(['development', 'test', 'production']),
+  }),
+);
